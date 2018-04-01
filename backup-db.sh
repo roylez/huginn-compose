d43302215b35e8f83d7c63316a691bfcac17deb8
roylez <me@roylez.info>
2018-04-02 08:35:31 +1000

    save backup for user

--- backup-db.sh
+++ backup-db.sh
@@ -17,6 +17,7 @@ LOCAL_BACKUP_DIR="./backup"
 
 # make database backup
 docker-compose exec db pg_dump -U postgres -Fc -d $1 -f "$CONTAINER_BACKUP_DIR/$YMD.db"
+docker-compose exec db chown 1000:1000 "$CONTAINER_BACKUP_DIR/$YMD.db"
 
 # delete backup files older than 7 days
 OLD=$(find $LOCAL_BACKUP_DIR -type f -mtime +7)
