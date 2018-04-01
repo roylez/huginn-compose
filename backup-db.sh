cef3898628a295aee6b94df7ce47db6e5ba084b5
roylez <me@roylez.info>
2018-04-01 22:07:07 +1000

    update backup script

--- backup-db.sh
+++ backup-db.sh
@@ -1,22 +1,21 @@
 #! /bin/bash
 # add this to cron
 #
-#   0 12 * * * /root/huginn/backup_db.sh huginn
+#   0 12 * * * $HOME/huginn/backup_db.sh huginn
 #
 # to restore:
 #   gunzip XXX.db.gz
-#   docker-compose exec db pg_restore -C -d <dbname> < XXX.db
+#   docker-compose exec db pg_restore -Fc -C -d huginn /backup/XXX.db
 
 # directory to save backups in, must be rwx by postgres user
-BASE_DIR="/var/backups/postgres"
+BASE_DIR="/backup"
 YMD=$(date "+%Y-%m-%d")
-mkdir -p $BASE_DIR
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
 # make database backup
-docker-compose exec db pg_dump -U postgres -Fc $1 | gzip -9 > "$BASE_DIR/$YMD.db.gz"
+docker-compose exec db pg_dump -U postgres -Fc -d $1 -f "$BASE_DIR/$YMD.db"
 
 # delete backup files older than 7 days
 OLD=$(find $BASE_DIR -type f -mtime +7)
